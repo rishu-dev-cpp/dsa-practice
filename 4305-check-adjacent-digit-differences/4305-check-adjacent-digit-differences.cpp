011880e0bd528022215a6d58a1107ad699540fd3
Rishu Kumar Pandey <richrishu69@gmail.com>
2026-05-17 20:13:59 +0530

    4305-CHECK-ADJACENT-DIGIT-DIFFERENCES Stats: Time: 0 ms (100%), Space: 8.6 MB (48.35%) - LeetHub

---
+++ 4305-check-adjacent-digit-differences/4305-check-adjacent-digit-differences.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isAdjacentDiffAtMostTwo(string s) {
+        for(int i=0;i<s.length()-1;i++){
+            int a = s[i]-'0';
+            int b = s[i+1]-'0';
+            if(abs(a-b)>2) return false;
+        }return true;
+    }
+};
