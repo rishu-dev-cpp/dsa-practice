36f8c65822f424214c0f6bf71beb4e452a4ed0ea
Rishu Kumar Pandey <richrishu69@gmail.com>
2026-05-20 12:52:38 +0530

    0240-SEARCH-A-2D-MATRIX-II Stats: Time: 41 ms (92.9%), Space: 18.7 MB (37.07%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m = matrix.size();
+     /* int m = matrix.size();
 
         int n = matrix[0].size();
 
@@ -24,5 +24,23 @@ public:
             }
 
         }return false;
+        */
+        int m = matrix.size();
+        int n = matrix[0].size();
+        
+        int row = m - 1; 
+        int col = 0;    
+        
+        while (row >= 0 && col < n) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] > target) {
+                row--; 
+            } else {
+                col++; 
+            }
+        }
+        
+        return false;
     }
 };
