fe00d6dadc0220621e01a311eaaaf9e6e8dbbb00
Rishu Kumar Pandey <richrishu69@gmail.com>
2026-05-20 21:48:18 +0530

    0378-KTH-SMALLEST-ELEMENT-IN-A-SORTED-MATRIX Stats: Time: 0 ms (100%), Space: 17 MB (94.96%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,13 +1,46 @@
 class Solution {
 public:
+
+    //StairCase jaise, Like search in 2D sorted matrix
+    int countLessEqual(vector<vector<int>>& matrix,int mid,int n){
+        int count = 0;
+        int row = 0;
+        int col = n -1;
+
+        while(row<n && col>=0){
+            if(matrix[row][col]<=mid){
+                count += (col + 1);
+                row++;
+            }else col--;
+        }
+        return count;
+    }
+
     int kthSmallest(vector<vector<int>>& matrix, int k) {
-        priority_queue<int> pq;
+    /*  priority_queue<int> pq;
         int m = matrix.size(), n = matrix[0].size();
         for(int i = 0;i<m;i++){
             for(int j = 0;j<n;j++){
                 pq.push(matrix[i][j]);
                 if(pq.size()>k) pq.pop();
             }
-        }return pq.top();
+        }return pq.top(); 
+    */
+        int size = matrix.size();
+
+        int low = matrix[0][0];
+        int  high = matrix[size-1][size-1];
+        int ans = low;
+
+        while(low<=high){
+            int mid= low+(high-low)/2;
+            if (countLessEqual(matrix, mid, size) >= k) {
+                ans = mid;      // Yeh potential answer ho sakta hai
+                high = mid - 1; // Aur chhota answer dhoondhne left jao
+            } else {
+                low = mid + 1;  // Agar log kam padh gaye, toh right jao
+            }
+        }
+        return ans;
     }
 };
