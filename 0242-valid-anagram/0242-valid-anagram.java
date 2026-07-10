e8b1ce83be66de938184af60c26b026c4a780318
Rishu Kumar Pandey <richrishu69@gmail.com>
2026-07-10 21:35:45 +0530

    0242-VALID-ANAGRAM Stats: Time: 6 ms (43.57%), Space: 44.1 MB (89.67%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length() != t.length()){
+            return false;
+        }
+        
+        int[] arr = new int[26];
+        
+        for(int i = 0; i< s.length(); i++){
+            int c = s.charAt(i);
+            arr[c-'a']++;
+            int k = t.charAt(i);
+            arr[k-'a']--;
+
+        }
+
+        for(int x : arr){
+            if(x != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
