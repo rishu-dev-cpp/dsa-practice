09c697426e65733de19c48ee3392d48e0ef51070
Rishu Kumar Pandey <richrishu69@gmail.com>
2026-09-11 01:21:00 +0530

    0300-LONGEST-INCREASING-SUBSEQUENCE Stats: Time: 37 ms (74.73%), Space: 46.2 MB (44.36%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -57,7 +57,7 @@ class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
         int[] dp = new int[n];
-        Arrays.fill(dp, 1);
+        Arrays.fill(dp, 1); // min length 1 hi rhega isliye 1 fill kiya 
         int maxLis = 1;
 
         for (int i = 0; i < n; i++) {
