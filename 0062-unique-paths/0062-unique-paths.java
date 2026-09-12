de27b569bc90ccb41799d89babbfcf9b37d9006f
Rishu Kumar Pandey <richrishu69@gmail.com>
2026-09-12 19:41:43 +0530

    0062-UNIQUE-PATHS Stats: Time: 0 ms (100%), Space: 42.4 MB (12.75%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -16,26 +16,53 @@ class Solution {
     }
 }
 */
+// DP bottom - up
+/*
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                if (r == 0 && c == 0) {
+                    dp[r][c] = 1;
+                } else {
+                    int up = (r > 0) ? dp[r - 1][c] : 0;
+                    int left = (c > 0) ? dp[r][c - 1] : 0;
+                    dp[r][c] = up + left;
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
+*/
 // DP top - down
 class Solution {
     int[][] dp;
+
     public int uniquePaths(int m, int n) {
         dp = new int[m][n];
-        for(int[] it : dp) {
+        for (int[] it : dp) {
             Arrays.fill(it, -1);
         }
 
         return help(m, n, 0, 0);
     }
-    private int help(int m, int n, int r, int c){
+
+    private int help(int m, int n, int r, int c) {
         int count = 0;
-        if(r == m || c == n) return 0;
-        if( r == m -1 && c == n -1) return 1;
-        if(dp[r][c] != -1) return dp[r][c];
+        if (r == m || c == n)
+            return 0;
+        if (r == m - 1 && c == n - 1)
+            return 1;
+        if (dp[r][c] != -1)
+            return dp[r][c];
 
         count += help(m, n, r + 1, c);
         count += help(m, n, r, c + 1);
-        
+
         return dp[r][c] = count;
     }
 }
+
