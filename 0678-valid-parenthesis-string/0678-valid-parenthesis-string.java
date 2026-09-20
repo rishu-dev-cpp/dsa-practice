71e4be37585e90aefc8c9e613eec60024ef8216f
Rishu Kumar Pandey <richrishu69@gmail.com>
2026-09-20 23:55:12 +0530

    0678-VALID-PARENTHESIS-STRING Stats: Time: 0 ms (100%), Space: 42.6 MB (82.7%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -1,27 +1,32 @@
 class Solution {
     public boolean checkValidString(String s) {
-        int minOpen = 0, maxOpen = 0;
-        for(char c : s.toCharArray()) {
-            if(c == '('){
+        int minOpen = 0; // Min possible open '(' count
+        int maxOpen = 0; // Max possible open '(' count
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
                 minOpen++;
                 maxOpen++;
-            }
-            else if(c == ')'){
+            } else if (c == ')') {
                 minOpen--;
                 maxOpen--;
-            }
-            else {
-                minOpen--;
-                maxOpen++;
+            } else { // c == '*'
+                minOpen--; // Treat '*' as ')'
+                maxOpen++; // Treat '*' as '('
             }
 
-            if(maxOpen < 0){
+            // Guard 1: Too many ')' -> even maximum '(' can't fix it
+            if (maxOpen < 0) {
                 return false;
             }
-            if(minOpen < 0) {
+
+            // Guard 2: minOpen can't drop below 0 (we can just treat '*' as empty string)
+            if (minOpen < 0) {
                 minOpen = 0;
             }
         }
+
+        // Return true if minOpen reaches 0 (all '(' can be balanced)
         return minOpen == 0;
     }
 }
